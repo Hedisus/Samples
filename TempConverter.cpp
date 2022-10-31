e1a643c0512c3dc5307a58f562f583070647a0af
Joseph Moore <54038936+Hedisus@users.noreply.github.com>
2022-10-31 13:53:36 -0400

    Update TempConverter.cpp

--- TempConverter.cpp
+++ TempConverter.cpp
@@ -1,6 +1,6 @@
 /*
  * Learning to use double and Floating-point numbers for fun and games
- *By converting temperature from Celsius to Useless
+ *By converting temperature from Celsius to Fahrenheit
  */
  #include <iostream>
  using namespace std;
@@ -13,7 +13,7 @@
     fahrenheit = celsius * 9.0 / 5.0 + 32.0;
     cout << celsius << " degree C is " << fahrenheit << " degree F." << endl << endl;
 
-    cout << "Enter the temp in that useless measurement: ";
+    cout << "Enter the temp in that American measurement: ";
     cin >> fahrenheit;
     celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
     cout << fahrenheit << " degree F is " << celsius << "degree C." << endl;
