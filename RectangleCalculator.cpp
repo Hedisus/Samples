326232aa8265ce76ee3c7ed5de1d7f5359a99b12
Joseph Moore <54038936+Hedisus@users.noreply.github.com>
2022-10-31 13:52:22 -0400

    Update RectangleCalculator.cpp

--- RectangleCalculator.cpp
+++ RectangleCalculator.cpp
@@ -11,19 +11,17 @@
 	
     cout << "Enter the length: ";                 //demands the length from the user
 	cin >> length;                                //Assigns the user input to length
-	cout << "How wide are you-- I mean how wide is the rectangle?";
+	cout << "How wide is the rectangle?";
 	cin >> width;								  //Assigns input to width
 	
-	//Time to compute this shit!!!
+	//Time to compute this!!!
 	area = length * width;
 	perimeter = length + width + length + width;
 	
 	//Printing time!!!!
-	cout << "Took you long enough to type that. Did you need a ruler to measure your ruler?" << endl ;
 	cout << "The area is: " << area << endl ;
-	cout << "No, I didn't need to make fun of you to buy myself time to compute! Humans..... sheesh" << endl ;
 	cout << "The Perimeter would be: " << perimeter << endl << "I think................" << endl ;
-	cout << "Whatever, I'm done with this peeper show. Creep you later!" << endl ;
+	cout << "That's Everything! Run me again sometime." << endl ;
 	
 	return 0;
 }	
